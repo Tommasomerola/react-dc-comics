c538a6669b04f7360437da1453f190dbbb66938a
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-17 18:10:00 +0100

    fai Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,61 @@
 export default function Footer() {
-    return <footer><h1>Footer</h1></footer>;
+    return (
+        <footer>
+            <div className="up-container">
+
+                <div className="container">
+                    
+                    <div className="sub-container-left">
+                    <ul>
+                            <li>DC COMICS</li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li> TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <ul>
+                            <li>DC</li>
+                            <li> Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                        <ul>
+                            <li>SITES</li>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                        <ul>
+                            <li>SHOP</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+
+                    <div className="sub-container-right">
+
+                    </div>
+
+                </div>
+
+                <div className="down-container">
+
+                </div>
+                
+            </div>
+        </footer>
+    )
   }
