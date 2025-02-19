43376c79d8ccf21d97fce743c94ba38f01d8bedd
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-19 10:24:14 +0100

    fai HeaderNav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+// imporiamo header nav 
+import HeaderNav from "./HeaderNav";
+
 // Contenuto Header
 
 export default function Header(){
@@ -7,22 +10,9 @@ export default function Header(){
                 <figure>
                     <img src="./img/dc-logo.png" alt="" />
                 </figure>
-                <nav>
-                    <ul>
 
-                      <li><a href="#">CHARACTERS</a></li>
-                      <li><a href="#">COMICS</a></li>
-                      <li><a href="#">MOVIES</a></li>
-                      <li><a href="#">TV</a></li>
-                      <li><a href="#">GAMES</a></li>
-                      <li><a href="#">COLLECTIBLES</a></li>
-                      <li><a href="#">VIDEOS</a></li>
-                      <li><a href="#">FANS</a></li>
-                      <li><a href="#">NEWS</a></li>
-                      <li><a href="#">SHOP</a></li>
-
-                    </ul>
-                </nav>
+                <HeaderNav/>
+               
             </div>
         </header>
     );
---
+++ src/components/HeaderNav.jsx
@@ -0,0 +1,31 @@
+// incollo la lista di links 
+
+const linksHeader = [
+    { id: 1, text: "CHARACTERS", url: "#", current: false },
+    { id: 2, text: "COMICS", url: "#", current: true },
+    { id: 3, text: "MOVIES", url: "#", current: false },
+    { id: 4, text: "TV", url: "#", current: false },
+    { id: 5, text: "GAMES", url: "#", current: false },
+    { id: 6, text: "COLLECTIBLES", url: "#", current: false },
+    { id: 7, text: "VIDEOS", url: "#", current: false },
+    { id: 8, text: "FANS", url: "#", current: false },
+    { id: 9, text: "NEWS", url: "#", current: false },
+    { id: 10, text: "SHOP", url: "#", current: false }
+];
+
+// esportiamo poi la funzione
+
+export default function HeaderNav() {
+    return(
+        <>
+           <nav>
+            {/* cicliamo con map ottenendno un array apposito */}
+                <ul>
+                    {linksHeader.map((link) =>
+                        <li><a href={link.url}><span>{link.text}</span></a></li>
+                    )}
+                </ul>
+            </nav>
+        </>
+    )
+}
